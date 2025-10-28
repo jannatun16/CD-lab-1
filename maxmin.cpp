e49a5d05b3699c28ade0dc422d2674d4bf793a38
jannatun16 <zamanjannatun@gmail.com>
2025-10-28 12:38:25 +0600

    lab 2

---
+++ maxmin.cpp
@@ -0,0 +1,24 @@
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter:";
+    cin>>n;
+     int arr[n];
+    cout<<"enter"<<n<<"number";
+    for(int i=0;i<n;i++){
+     cin>>arr[i];}
+     int max=arr[0];
+      int min=arr[0];
+       for(int i=1;i<n;i++){
+        if(arr[i]>max)
+        max=arr[i];
+        if(arr[i]<min)
+            min=arr[i];
+       }
+       cout<<"max number:"<<max<<endl;
+        cout<<"min number:"<<min<<endl;
+
+    return 0;
+}
